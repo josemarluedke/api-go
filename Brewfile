496a133207e4038ee9c10adb02beb6e1143e55bd
Josemar Luedke <josemarluedke@gmail.com>
2017-09-29 21:37:59 -0700

    WIP proto

---
+++ Brewfile
@@ -0,0 +1,7 @@
+###############################################################################
+# HOW TO USE THIS FILE:
+#   Install homebrew-bundle with `brew tap Homebrew/bundle`
+#   then `brew bundle --verbose` where the Brewfile is located.
+###############################################################################
+
+brew "protobuf"
---
+++ main.go
@@ -0,0 +1,16 @@
+package main
+
+// func main() {
+// // // Insert
+// userToCreate := &models.User{Name: "admin"}
+// err := db.Conn.Insert(userToCreate)
+// if err != nil {
+// panic(err)
+// }
+// fmt.Println(userToCreate.Id, userToCreate.Name)
+
+// // Find
+// foundUser := models.User{Id: 1}
+// db.Conn.Select(&foundUser)
+// fmt.Println(foundUser)
+// }
